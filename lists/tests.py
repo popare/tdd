80dded354807294cff115d7e03b81daf5e1324eb
unknown <popare123@gmial.com>
2014-05-05 15:56:16 -0400

    functonal test now checks when input to-do list

--- lists/tests.py
+++ lists/tests.py
@@ -3,21 +3,21 @@ from django.test import TestCase
 # Create your tests here.
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.template.loader import render_to_string
 from django.http import HttpRequest
 
 from lists.views import home_page
 
 
+
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-
+        found = resolve('/')  #2
+        self.assertEqual(found.func, home_page)  #3
+ 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  #1
-        response = home_page(request)  #2
-        self.assertTrue(response.content.startswith(b'<html>'))  #3
-        self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
-        self.assertTrue(response.content.endswith(b'</html>'))  #5
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
