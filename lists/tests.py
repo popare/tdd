69ca54e722ef329c018d657af191aab149c5be48
unknown <popare123@gmial.com>
2014-05-05 15:24:47 -0400

    ch03 urls_py

---
+++ lists/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from lists.views import home_page #1
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  #2
+        self.assertEqual(found.func, home_page)  #3
